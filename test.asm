942e3356bf86a50673502cf6c55229566c2630f5
Ben Brannick <ben.brannick13@imperial.ac.uk>
2014-04-29 18:50:08 +0100

    Added additional methods

--- test.asm
+++ test.asm
@@ -164,3 +164,16 @@ L99:
        pop  rax
        pop  rbp
        ret
+
+; ---------------------------------------------------------------------
+;                    ADDITIONAL METHODS
+
+CR      equ     13              ; carriage-return
+LF      equ     10              ; line-feed
+TAB     equ     9               ; tab
+MINUS   equ     '-'             ; minus
+
+LINUX   equ     80H             ; interupt number for entering Linux kernel
+EXIT    equ     1               ; Linux system call 1 i.e. exit ()
+WRITE   equ     4               ; Linux system call 4 i.e. write ()
+STDOUT  equ     1               ; File descriptor 1 i.e. standard output
